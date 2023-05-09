7ed1b3f14b767e1eec510cd783785872ed756c6d
Dorka Zsirai <dorka.zsirai@stud.uni-corvinus.hu>
2023-05-09 08:37:38 +0200

    Add project files.

---
+++ week9/Entities/BirthProbality.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week9.Entities
+{
+    public class BirthProbality
+    {
+
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public int Double P { get; set; }
+    } 
+
+}
---
+++ week9/Entities/DeathProbality.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week9.Entities
+{
+    public class DeathProbality
+    {
+        public int Gender Gender { get; set; }
+        public int Age { get; set; }
+        public int Double P { get; set; }
+    }
+}
